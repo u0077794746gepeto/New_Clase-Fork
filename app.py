2439e6725326eaf35c3a0d2c7cb2a48a5ffa3cc6
JMKonectia <juanmiguelkonectia@gmail.com>
2026-02-28 08:31:10 +0100

    Arreglo app

--- app.py
+++ app.py
@@ -27,6 +27,11 @@ def create_app():
     return app
 
 
+# if __name__ == "__main__":
+#     app = create_app()
+#     app.run(debug=True)
+
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
